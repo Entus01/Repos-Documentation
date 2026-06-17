38cda6fad26594e27bc41a2148ad1384a60e8411
Miguel Barrera <miguelbarrera.wd@gmail.com>
2026-06-17 18:12:24 -0500

    chore(project): update project name configuration

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -3,35 +3,32 @@ import "./Content.css";
 import { NAV_ITEMS } from "../../constants/navigation.js";
 
 export default function Content() {
-    return (
-        <main className="content">
-            <Routes>
-                <Route
-                    path="/"
-                    element={
-                        <article className="document document--empty">
-                            <h1>Virtual Library</h1>
-                            <p>
-                                Select a document from the catalog to begin
-                                reading.
-                            </p>
-                        </article>
-                    }
-                />
+  return (
+    <main className="content">
+      <Routes>
+        <Route
+          path="/Repos-Documentation"
+          element={
+            <article className="document document--empty">
+              <h1>Virtual Library</h1>
+              <p>Select a document from the catalog to begin reading.</p>
+            </article>
+          }
+        />
 
-                {NAV_ITEMS.map(({ id, path, name, document: Document }) => (
-                    <Route
-                        key={id}
-                        path={path}
-                        element={
-                            <article className="document">
-                                <h1>{name}</h1>
-                                <Document />
-                            </article>
-                        }
-                    />
-                ))}
-            </Routes>
-        </main>
-    );
+        {NAV_ITEMS.map(({ id, path, name, document: Document }) => (
+          <Route
+            key={id}
+            path={path}
+            element={
+              <article className="document">
+                <h1>{name}</h1>
+                <Document />
+              </article>
+            }
+          />
+        ))}
+      </Routes>
+    </main>
+  );
 }
